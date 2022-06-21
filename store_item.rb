90b7c2f96d31e0330bdb7e969aed4e44f1d6edf7
Maciej Jakubiak <maciej.z.jakubiak@gmail.com>
2022-06-21 14:27:20 -0500

    Store items in hash

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+#item1 = {:name => pants, :color => blue, :price => 50, :brand => nike}
+#item2 = {:name => shirt, :color => white, :price => 40, :brand => adidas}
+#item3 = {:name => shoes, :color => black, :price => 30, :brand => puma}
+
+
