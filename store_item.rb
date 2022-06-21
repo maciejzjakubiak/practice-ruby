ede28025bda114ff2343200af355d6635524d1ec
Maciej Jakubiak <maciej.z.jakubiak@gmail.com>
2022-06-21 15:07:34 -0500

    Store items in class

--- store_item.rb
+++ store_item.rb
@@ -3,3 +3,26 @@
 #item3 = {:name => shoes, :color => black, :price => 30, :brand => puma}
 
 
+class Items
+  def initialize(name_input, color_input, price_input, brand_input)
+    @name = name_input
+    @color = color_input
+    @price = price_input
+    @brand = brand_input
+  end
+  def name
+    @name
+  end
+  def color
+    @color
+  end
+  def price
+    @price
+  end
+  def brand
+    @brand
+  end
+end
+
+item1 = Items.new("CPU", "silver", "350", "intel")
+puts item1.color
