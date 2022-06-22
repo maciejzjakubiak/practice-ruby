d2c5b4bdb51426e74e4503bfbd1f3dc1687bcba0
Maciej Jakubiak <maciej.z.jakubiak@gmail.com>
2022-06-22 11:59:00 -0500

    Added and used attributes write and read

--- store_item.rb
+++ store_item.rb
@@ -2,27 +2,41 @@
 #item2 = {:name => shirt, :color => white, :price => 40, :brand => adidas}
 #item3 = {:name => shoes, :color => black, :price => 30, :brand => puma}
 
-
 class Items
-  def initialize(name_input, color_input, price_input, brand_input)
-    @name = name_input
-    @color = color_input
-    @price = price_input
-    @brand = brand_input
+  attr_reader :name, :color, :price, :brand
+  attr_writer :name, :color, :price, :brand
+
+  def initialize(input_options)
+    @name = input_options[:name]
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @brand = input_options[:brand]
   end
-  def name
-    @name
-  end
-  def color
-    @color
-  end
-  def price
-    @price
-  end
-  def brand
-    @brand
+
+  #  def name
+  #    @name
+  #  end
+
+  #  def color
+  #    @color
+  #  end
+
+  #  def price
+  #    @price
+  #  end
+
+  # def brand
+  #   @brand
+  # end
+  def prtint_info
+    puts "This is #{name} and it is made by #{brand}. It is #{color} and it cost #{price}"
   end
 end
 
-item1 = Items.new("CPU", "silver", "350", "intel")
-puts item1.color
+item1 = Items.new({ name: "CPU", color: "silver", price: "350", brand: "intel" })
+item2 = Items.new({ name: "Motherboard", color: "black", price: "170", brand: "Asus" })
+item3 = Items.new({ name: "SSD", color: "yellow", price: "85", brand: "Samsung" })
+
+item1.prtint_info
+item2.prtint_info
+item3.prtint_info
